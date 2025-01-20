fb426c6ff71e98954bf127b0460571fb046cf4ea
William Adams <willadams@aol.com>
2025-01-20 07:52:53 -0500

    update gcpdxf for nimport

--- gcpdxf.py
+++ gcpdxf.py
@@ -1,11 +1,13 @@
+from openscad import *
+#nimport("https://raw.githubusercontent.com/WillAdams/gcodepreview/refs/heads/main/gcodepreview.py")
 from gcodepreview import *
 
-gcp = gcodepreview(False, #generatescad
+gcp = gcodepreview(False, #generatepaths
                    False, #generategcode
                    True #generatedxf
                    )
 
-Base_filename = "export"
+Base_filename = "dxfexport"
 large_square_tool_num = 102
 small_square_tool_num = 0
 large_ball_tool_num = 0
@@ -18,23 +20,35 @@ Roundover_tool_num = 0
 MISC_tool_num = 0
 
 gcp.opendxffile(Base_filename)
-gcp.opendxffiles(Base_filename,
-                 large_square_tool_num,
-                 small_square_tool_num,
-                 large_ball_tool_num,
-                 small_ball_tool_num,
-                 large_V_tool_num,
-                 small_V_tool_num,
-                 DT_tool_num,
-                 KH_tool_num,
-                 Roundover_tool_num,
-                 MISC_tool_num)
+#gcp.opendxffiles(Base_filename,
+#                 large_square_tool_num,
+#                 small_square_tool_num,
+#                 large_ball_tool_num,
+#                 small_ball_tool_num,
+#                 large_V_tool_num,
+#                 small_V_tool_num,
+#                 DT_tool_num,
+#                 KH_tool_num,
+#                 Roundover_tool_num,
+#                 MISC_tool_num)
 
 gcp.dxfrectangle(large_square_tool_num, 0, 0, 100, 50)
-gcp.dxfrectangleround(large_square_tool_num, 1, 1, 98, 48, 11)
+
+gcp.dxfarc(large_square_tool_num,  3,  3, 6,   0,  90)
+gcp.dxfarc(large_square_tool_num, 97,  3, 6,  90, 180)
+gcp.dxfarc(large_square_tool_num, 97, 47, 6, 180, 270)
+gcp.dxfarc(large_square_tool_num,  3, 47, 6, 270, 360)
+
+gcp.dxfline(large_square_tool_num,  3, 9, 3,41)
+gcp.dxfline(large_square_tool_num,  9, 3,91, 3)
+gcp.dxfline(large_square_tool_num, 97, 9,97,41)
+gcp.dxfline(large_square_tool_num,  9,47,91,47)
+
+gcp.dxfrectangleround(large_square_tool_num, 12, 6, 24, 36, 6)
+gcp.dxfrectangleround(large_square_tool_num, 64, 6, 24, 36, 6)
 
 gcp.dxfcircle(large_square_tool_num, 50, 25, 12)
 
-gcp.closedxffiles()
+#gcp.closedxffiles()
 gcp.closedxffile()
 
