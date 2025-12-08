0d83c021a4c1785797512cfb7271ac27931aea6b
William Adams <willadams@aol.com>
2025-12-08 09:41:25 -0500

    Rename instance from ttdm to tdmt

---
+++ tdmt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#import math
+
+from openscad import *
+
+from threeDmodelturtle import *
+
+tdmt = threeDmodelturtle()
+
+tdmt.left(359)
+
+tdmt.forward(40)
+
+tdmt.incline(45)
+
+tdmt.left(91)
+
+tdmt.forward(40)
+
+tdmt.decline(90)
+
+tdmt.left(90)
+
+tdmt.forward(40)
+
+tdmt.showmodel()
+        
